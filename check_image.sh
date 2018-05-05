c0fe9da3a5ce57ff869d16d389dbb1c7d9c27c01
David Weber <dave@horn.co>
2018-05-05 17:11:24 +0200

    Use distributed connect and add test for upness

--- check_image.sh
+++ check_image.sh
@@ -26,7 +26,7 @@ if (( $? != 0 )); then
 fi
 
 # Check Zookeeper
-echo -n "Zookeeper:          "
+echo -n "Zookeeper:           "
 if (( $(echo dump | nc $address 2181 | grep broker | wc -l) == 1 )); then
   echo $ok
 else
@@ -34,7 +34,7 @@ else
 fi
 
 # Check if Cassandra is running
-echo -n "Kafka broker:       "
+echo -n "Kafka broker:        "
 nc -z $address 9092
 if (( $? == 0 )); then
   echo $ok
@@ -43,7 +43,7 @@ else
 fi
 
 # Check grafana
-echo -n "Grafana:            "
+echo -n "Grafana:             "
 if [[ $(curl -Is http://$address:3000/login | head -1) = *'HTTP/1.1 200 OK'* ]]; then
   echo $ok
 else
@@ -51,7 +51,7 @@ else
 fi
 
 # Check elastic search
-echo -n "ElasticSearch:      "
+echo -n "ElasticSearch:       "
 if [[ $(curl -Is http://$address:9200 | head -1) = *'HTTP/1.1 200 OK'* ]]; then
   echo $ok
 else
@@ -59,7 +59,7 @@ else
 fi
 
 # Check the schema registry
-echo -n "Schema registry:    "
+echo -n "Schema registry:     "
 if [[ $(curl -Is http://$address:8081/subjects | head -1) = *'HTTP/1.1 200 OK'* ]]; then
   echo $ok
 else
@@ -67,7 +67,7 @@ else
 fi
 
 # Check the schema registry
-echo -n "KSQL server:        "
+echo -n "KSQL server:         "
 if [[ $(curl -Is http://$address:8088/status | head -1) = *'HTTP/1.1 200 OK'* ]]; then
   echo $ok
 else
@@ -75,15 +75,23 @@ else
 fi
 
 # Check the Kafka ReST interface
-echo -n "Kafka ReST server:  "
+echo -n "Kafka ReST server:   "
 if [[ $(curl -Is http://$address:8082 | head -1) = *'HTTP/1.1 200 OK'* ]]; then
   echo $ok
 else
   echo $failed
 fi
 
+# Check the Kafka ReST interface
+echo -n "Connect ReST server: "
+if [[ $(curl -Is http://$address:8083/connectors | head -1) = *'HTTP/1.1 200 OK'* ]]; then
+  echo $ok
+else
+  echo $failed
+fi
+
 # Check if Cassandra is running
-echo -n "Cassandra server:   "
+echo -n "Cassandra server:    "
 nc -z $address 9042
 if (( $? == 0 )); then
   echo $ok
