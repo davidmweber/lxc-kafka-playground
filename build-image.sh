17c7b5941d8dd8e717923992c1005dd1f3498be8
David Weber <dave@horn.co>
2018-05-10 16:44:59 +0200

    Update to ElasticSearch 6 and pull in Landoop Cassandra and Elastic Search connectors.

--- build-image.sh
+++ build-image.sh
@@ -34,6 +34,10 @@ lxc exec $1 systemctl restart cassandra
 lxc file push --uid=0 --gid=0 --mode=644 configs/ksql-server.properties $1/etc/ksql/ksql-server.properties
 lxc file push --uid=0 --gid=0 --mode=644 configs/connect-no-schema.properties $1/etc/kafka/connect-distributed.properties
 
+# Grab the landoop connectors for Cassandra and ElasticSearch
+lxc exec kafka-streams -- /bin/bash -c "cd /usr/share/java && curl -sL https://github.com/Landoop/stream-reactor/releases/download/1.0.0/kafka-connect-cassandra-1.0.0-1.0.0-all.tar.gz | tar xzf -"
+lxc exec kafka-streams -- /bin/bash -c "cd /usr/share/java && curl -sL https://github.com/Landoop/stream-reactor/releases/download/1.0.0/kafka-connect-elastic-1.0.0-1.0.0-all.tar.gz | tar xzf -"
+
 # Copy the systemd control scripts over to the container and enable the confluent services
 cd configs/systemd
 find ./ -name "*.service" -exec lxc file push --uid=0 --gid=0 --mode=644 {} $1/lib/systemd/system/ \;
--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@
 export DEBIAN_FRONTEND=noninteractive
 
 # We use es 5.x because there is an issue with Kakfa streams connector for es v 6.x
-echo 'deb https://artifacts.elastic.co/packages/5.x/apt stable main' | tee -a /etc/apt/sources.list.d/elastic-5.x.list
+echo 'deb https://artifacts.elastic.co/packages/6.x/apt stable main' | tee -a /etc/apt/sources.list.d/elastic-5.x.list
 echo 'deb https://packagecloud.io/grafana/stable/debian/ jessie main' | tee -a /etc/apt/sources.list.d/grafana.list
 echo "deb http://www.apache.org/dist/cassandra/debian 311x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
 add-apt-repository 'deb [arch=amd64] https://packages.confluent.io/deb/4.1 stable main'
-curl https://packages.confluent.io/deb/4.1/archive.key | apt-key add -
-curl https://packagecloud.io/gpg.key | apt-key add -
-curl https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
-curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
+curl -s https://packages.confluent.io/deb/4.1/archive.key | apt-key add -
+curl -s https://packagecloud.io/gpg.key | apt-key add -
+curl -s https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
+curl -s https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
 apt -y update
 apt -y upgrade
 apt -y install openjdk-8-jre-headless
