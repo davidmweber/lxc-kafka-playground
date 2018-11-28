dc40b5f85ab2fc39beb33adf473330de94c28808
David Weber <dave@horn.co>
2018-11-28 10:20:50 +0200

    Updates for confluent v5.0.x. Needs check for failed install script components

--- build-image.sh
+++ build-image.sh
@@ -30,17 +30,13 @@ lxc exec $1 systemctl restart elasticsearch
 lxc file push --uid=0 --gid=0 --mode=644 configs/cassandra.yaml $1/etc/cassandra/cassandra.yaml
 lxc exec $1 systemctl restart cassandra
 
-# Configure confluent tools. Note connect runs with schema's disabled.
 lxc file push --uid=0 --gid=0 --mode=644 configs/ksql-server.properties $1/etc/ksql/ksql-server.properties
-lxc file push --uid=0 --gid=0 --mode=644 configs/connect-no-schema.properties $1/etc/kafka/connect-distributed.properties
+lxc exec $1 systemctl restart confluent-ksql
+
+lxc file push --uid=0 --gid=0 --mode=644 configs/schema-registry.properties $1/etc/schema-registry/schema-registry.properties
+lxc exec $1 systemctl restart confluent-schema-registry
+
 
 # Grab the landoop connectors for Cassandra and ElasticSearch
-lxc exec kafka-streams -- /bin/bash -c "cd /usr/share/java && curl -sL https://github.com/Landoop/stream-reactor/releases/download/1.0.0/kafka-connect-cassandra-1.0.0-1.0.0-all.tar.gz | tar xzf -"
-lxc exec kafka-streams -- /bin/bash -c "cd /usr/share/java && curl -sL https://github.com/Landoop/stream-reactor/releases/download/1.0.0/kafka-connect-elastic-1.0.0-1.0.0-all.tar.gz | tar xzf -"
+#lxc exec kafka-streams -- /bin/bash -c "cd /usr/share/java && curl -sL https://github.com/Landoop/stream-reactor/releases/download/1.0.0/kafka-connect-cassandra-1.0.0-1.0.0-all.tar.gz | tar xzf -"
 
-# Copy the systemd control scripts over to the container and enable the confluent services
-cd configs/systemd
-find ./ -name "*.service" -exec lxc file push --uid=0 --gid=0 --mode=644 {} $1/lib/systemd/system/ \;
-lxc exec $1 systemctl enable `ls -1 *.service`
-lxc exec $1 systemctl start `ls -1 *.service`
-cd -
--- install.sh
+++ install.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
+CONFLUENT_V=5.0
 
-echo 'deb https://artifacts.elastic.co/packages/6.x/apt stable main' | tee -a /etc/apt/sources.list.d/elastic-5.x.list
-echo 'deb https://packagecloud.io/grafana/stable/debian/ stretch main' | tee -a /etc/apt/sources.list.d/grafana.list
+echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-5.x.list
+echo "deb https://packagecloud.io/grafana/stable/debian/ stretch main" | tee -a /etc/apt/sources.list.d/grafana.list
 echo "deb http://www.apache.org/dist/cassandra/debian 311x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
-add-apt-repository 'deb [arch=amd64] https://packages.confluent.io/deb/4.1 stable main'
-curl -s https://packages.confluent.io/deb/5.0/archive.key | apt-key add -
+add-apt-repository "deb [arch=amd64] https://packages.confluent.io/deb/${CONFLUENT_V} stable main"
+curl -s https://packages.confluent.io/deb/${CONFLUENT_V}/archive.key | apt-key add -
 curl -s https://packagecloud.io/gpg.key | apt-key add -
 curl -s https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
 curl -s https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
 apt -y update
 apt -y upgrade
-apt -y install openjdk-8-jre-headless
-apt -y install confluent-platform-oss-2.11
-apt -y install elasticsearch
-apt -y install grafana
-apt -y install cassandra
+apt -y install openjdk-8-jre-headless confluent-platform-oss-2.11 elasticsearch grafana cassandra
 apt -y autoremove
 systemctl enable elasticsearch
 systemctl start elasticsearch
@@ -24,3 +21,15 @@ systemctl enable grafana-server
 systemctl start grafana-server
 systemctl enable cassandra
 systemctl start cassandra
+systemctl enable confluent-zookeeper
+systemctl start confluent-zookeeper
+systemctl enable confluent-kafka
+systemctl start confluent-kafka
+systemctl enable confluent-kafka-connect
+systemctl start confluent-kafka-connect
+systemctl enable confluent-schema-registry
+systemctl start confluent-schema-registry
+systemctl enable confluent-kafka-rest
+systemctl start confluent-kafka-rest
+systemctl enable confluent-ksql
+systemctl start confluent-ksql
